265d061de271a7c75fcbbe73ef9435734048dbe5
Kassim <kassimrooney@gmail.com>
2025-07-01 16:19:52 +0300

    updated createbooking in api

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,21 @@
+import axios from "axios";
+
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5001/api";
+
+export const fetchServices = async () => {
+  const res = await axios.get(`${API_BASE_URL}/services`);
+  return res.data;
+};
+
+export const createBooking = async (data) => {
+  const token = localStorage.getItem("token");
+  const res = await axios.post(`${API_BASE_URL}/bookings`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.data;
+};
 
 export const loginUser = async (data) => {
   const res = await axios.post(`${API_BASE_URL}/auth/login`, data);
